e3544006982a738099af678a64690bc669fc96bb
KalosCreates <58001923+KalosCreates@users.noreply.github.com>
2021-03-05 13:20:40 +0400

    Update host.js

--- command/info/host.js
+++ command/info/host.js
@@ -2,7 +2,7 @@ const { RichEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 
 module.exports = {
-    name: "host",
+    name: "privsay",
     category: "info",
     description: "Host something",
     run: async (client, message, args) => {
@@ -14,8 +14,8 @@ module.exports = {
             .setURL("https://pornhub.com") 
             .setTitle(splitted[0])
             .addField("coon", splitted[1], true)
-            .addField("nigger", message.author, true)
-            .addField("faggot", stripIndents`**nigger coon**`, false)
+            .addField("I cannot breath", message.author, true)
+            .addField("I cannot breath", stripIndents`**I cannot breath**`, false)
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
             .setColor("#36393F")
 
