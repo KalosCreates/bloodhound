8bded7cc193dac0b2d1c6ef8c842bbc30ecf3134
KalosCreates <58001923+KalosCreates@users.noreply.github.com>
2021-03-05 13:00:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on("ready", () => {
         
         status: "online",
         game: {
-            name: "#FeelTheAdrenaline",
+            name: "Derek Chauvin",
             type: "PLAYING"
         }
     }); 
