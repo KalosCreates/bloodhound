79ea18491670a57ba4063d61ee9b3816affec561
KalosCreates <58001923+KalosCreates@users.noreply.github.com>
2021-03-05 13:17:04 +0400

    Update host.js

--- command/info/host.js
+++ command/info/host.js
@@ -13,15 +13,9 @@ module.exports = {
         const embed = new RichEmbed()
             .setURL("https://pornhub.com") 
             .setTitle(splitted[0])
-            .addField("Custom key", splitted[1], true)
-            .addField("Host", message.author, true)
-            .addField("Rules", stripIndents`**YOU MAY** fight if contested off spawn at your drop
-            **DO NOT** fight or shoot at all until 2nd zone has fully closed
-            **DO NOT** Stream snipe/use anonymous or streamer mode
-            **DO NOT** grief players in any way until the 2nd zone has fully closed
-            \n
-            **GRIEFING** including but is not limited to:
-            breaking or shooting structures/vehicles, stealing loot/vehicles or building on top of eachother`, false)
+            .addField("coon", splitted[1], true)
+            .addField("nigger", message.author, true)
+            .addField("faggot", stripIndents`**nigger coon**`, false)
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
             .setColor("#36393F")
 
